57a8a4e7da062bbfdccc842de62b6cbec95439c3
nhatquangta2911 <nhatquangta2911@gmail.com>
2019-12-05 21:15:25 +0700

    Predict & Train

--- ai_model/views.py
+++ ai_model/views.py
@@ -1,3 +1,15 @@
+#IMPORT LIBRARY
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+from imblearn.over_sampling import SMOTE
+from sklearn.preprocessing import MinMaxScaler
+import warnings
+from collections import Counter
+warnings.filterwarnings('ignore')
+from keras import Sequential
+from keras.layers import Dense
 from rest_framework.decorators import api_view
 # from django.core import serializers
 from rest_framework.response import Response
@@ -8,31 +20,49 @@ from .models import Result
 from .serializers import ResultSerializers
 import pickle
 from sklearn.externals import joblib
-# import json
-import numpy as np
-import pandas as pd
+import json
 
 @api_view(["POST"])
 def predict(request):
     try:
-        # mdl = joblib.load("./question_model.pkl")
+        model = joblib.load("./question_model.pkl")
         mydata = request.data
-        # unit = np.array(list(mydata.values()))
-        # unit = unit.reshape(1, -1)
-        # X = mdl.transform(unit)
-        # y_pred = mdl.predict(X)
-        # y_pred = (y_pred > 0.5)
-        # newdf = pd.DataFrame(y_pred, columns=['Result'])
-        # newdf = newdf.replace({True: 'Positive', False: 'Negative'})
-        # newdf = serializers.serialize('json', newdf)
-        return JsonResponse(mydata, safe=False)
+        sc = MinMaxScaler()
+        w = np.array([[2, 4, 3, 5 ], [2, 4, 2, 2], [mydata['Question_Period'], mydata['Question_Type'], mydata['Question_Content_Type'], mydata['Job']]])
+        w = w.reshape(-3, 4)
+        X_test = sc.fit_transform(w)
+        y_pred = model.predict(X_test) 
+        return Response({ "result" : y_pred[2], "metadata": mydata, "w_after_reshape": w, "X_test": X_test })
     except ValueError as e:
         return Response(e.args[0], status.HTTP_400_BAD_REQUEST)
 
 @api_view(["POST"])
 def train(request):
     try:
-        
-        return JsonResponse('Training sucks',safe=False)
+        df = pd.read_csv('dataset2.csv')
+        df = df.dropna()
+        df.isna().any()
+        Counter(df['Answer_Result'])
+        pre_y = df['Answer_Result']
+        pre_X = df.drop('Answer_Result', axis=1)
+        dm_X = pd.get_dummies(pre_X)
+        dm_y = pre_y.map(dict(Positive=1, Negative=0))
+        smote = SMOTE(ratio='minority')
+        X1, y = smote.fit_sample(dm_X, dm_y)
+        sc = MinMaxScaler()
+        X = sc.fit_transform(X1)
+        Counter(y)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, shuffle=True)
+        myData = request.data
+        classifier = Sequential()
+        classifier.add(Dense(myData['layer1'], activation=myData['af1'], kernel_initializer='random_normal', input_dim=X_test.shape[1]))
+        classifier.add(Dense(myData['layer2'], activation=myData['af2'], kernel_initializer='random_normal'))
+        classifier.add(Dense(myData['layer3'], activation=myData['af3'], kernel_initializer='random_normal'))
+        classifier.add(Dense(1, activation='sigmoid', kernel_initializer='random_normal'))
+        classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+        classifier.fit(X_train, y_train, batch_size=20, epochs=myData['epochs'])
+        eval_model = classifier.evaluate(X_train, y_train)
+        joblib.dump(classifier, 'question_model.pkl')
+        return JsonResponse(myData, safe=False)
     except ValueError as e:
         return Response(e.args[0], status.HTTP_400_BAD_REQUEST)
